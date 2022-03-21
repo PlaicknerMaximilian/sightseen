fdd9fec7ec401b5f673d82bc3154ff70b8cb4d85
PlaicknerMaximilian <85285841+PlaicknerMaximilian@users.noreply.github.com>
2022-03-21 11:46:43 +0100

    Alles Dateien
    erster Upload

---
+++ Sightseen/website.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang=en>
+  <head>
+    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
+    <link rel="stylesheet" href="css/website.css">
+  </head>
+  <body>
+
+  <div class="header">
+    <h1>Sightseeing</h1>
+  </div>
+
+  <div class="navbar">
+    <a href="#" class="active">Home</a>
+    <a href="#">Link</a>
+    <a href="#">Link</a>
+    <a href="#" class="right">Logout</a>
+  </div>
+
+    <div class="footer">
+      <h2>Sightseeing</h2>
+      <p>Feichter Philipp, Plaickner Maximilian, Volgger Matthias, Weissteiner Josef</p>
+    </div>
+
+  </body>
+</html>
