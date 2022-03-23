51cf5a8e5a523cfd4ec6d9174afb7f1cdd6381d8
PhilippF3 <83636380+PhilippF3@users.noreply.github.com>
2022-03-23 15:25:43 +0100

    Versuch
    Karten zusammenfügen

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ let map;
 let l=0;
 let b=0; 
 let localContextMapView;
-
+var newPoint;
+var marker = null;
 function initMap() {
   localContextMapView = new google.maps.localContext.LocalContextMapView({
     element: document.getElementById("map"),
@@ -14,7 +15,7 @@ function initMap() {
   });
 
   navigator.geolocation.getCurrentPosition(function(position) {  
-  var newPoint = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  newPoint = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
   if(l==0){
     l=position.coords.latitude;  
     b=position.coords.longitude;
@@ -48,12 +49,12 @@ function initMap() {
     l=parseFloat(myArray[3].split(",")[0]);
     b=parseFloat(myArray[6].split("}")[0]);
     console.log(l);
-    infoWindow.open(map);
-
-    initMap();
-  });  
+    infoWindow.open(map);  
+    initMap();  
   });
-
+  });
+    
+  
 }
 
 
@@ -68,38 +69,29 @@ function initMap2() {
     ],
     maxPlaceCount: 12,
   });
-  
-  navigator.geolocation.getCurrentPosition(function(position) {  
-    var newPoint = new google.maps.LatLng(position.coords.latitude, 
-                                          position.coords.longitude);
-  l=position.coords.latitude;  
-  b=position.coords.longitude;       
-  
-  map = localContextMapView.map;
-  map.setOptions({
-    center: { lat: l, lng: b },
-    zoom: 12,
-  });
-  });
-  
-
-  
- 
-
-
-  var marker = null;
-
   function autoUpdate() {
     navigator.geolocation.getCurrentPosition(function(position) {  
-      var newPoint = new google.maps.LatLng(position.coords.latitude, 
-                                            position.coords.longitude);
+      newPoint = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    l=position.coords.latitude;  
+    b=position.coords.longitude;       
+    
+    map = localContextMapView.map;
+    map.setOptions({
+      center: { lat: l, lng: b },
+      zoom: 12,
+    });
+    });
+    navigator.geolocation.getCurrentPosition(function(position) {  
+      newpoint = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
       if (marker) {
         // Marker already created - Move it
+        console.log("aaa");
         marker.setPosition(newPoint);
       }
       else {
         // Marker does not exist - Create it
+        console.log("aaa2");
         marker = new google.maps.Marker({
           position: newPoint,
           map: map
--- website.php
+++ website.php
@@ -1,33 +1,26 @@
 <!DOCTYPE html>
 
 <?php
-    echo'<script async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIWilWThH0ONE_t3Nb1NPvML_LvK2ItCc&libraries=localContext&v=beta&callback=initMap">
-    </script>';
-    if(isset($_POST['add'])){
-        echo'<script async
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIWilWThH0ONE_t3Nb1NPvML_LvK2ItCc&libraries=localContext&v=beta&callback=initMap">
-</script>';
-    }
-    if(isset($_POST['add2'])){
-        echo'<script
-      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIWilWThH0ONE_t3Nb1NPvML_LvK2ItCc&callback=initMap2&v=weekly"
-      async
-    ></script>';
-    }
-    ?>
 
+    if(isset($_POST['add2'])){
+        echo'<script async
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIWilWThH0ONE_t3Nb1NPvML_LvK2ItCc&libraries=localContext&v=beta&callback=initMap2">
+        </script>;';
+    }
+
+    if(isset($_POST['add1'])){
+        echo'<script async
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAIWilWThH0ONE_t3Nb1NPvML_LvK2ItCc&libraries=localContext&v=beta&callback=initMap">
+        </script>;';
+    }
+
+?>
 <html lang=en>
   <head>
-    <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/leaflet.js"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="css/website.css">
+    <link rel="stylesheet" type="text/css" href="css/website.css">
     <link rel="stylesheet" type="text/css" href="css/style.css" />
     <script src="js/index.js"></script>
-    <link rel="shortcut icon" href="#">
   </head>
   <body>
 
@@ -44,9 +37,22 @@
 
   <div id="map"></div>
 
+  <c>Live Standort</c>
+  <label class="switch" id="check">
+  <input type="checkbox" id="check">
+  <span class="slider round" id="check"></span>
+  </label>
+
+  <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
   <form method="post" action="">
-    <span id="locationSpan">Live location: </span><input type="submit" value="On" name="on" id="on">
-    <input type="submit" value="Off" name="off">
+    <input type="submit" value="Ort suchen" name="add1" id="add1">
+    <input type="submit" value="Mich suchen" name="add2" id="add2">
   </form>
+  
+
+  
+
+
+
   </body>
 </html>
