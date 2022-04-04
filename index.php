6f84ae470bad00b95234ac6c78ebd1507ec36914
stweijos <stweijos@bx.fallmerayer.it>
2022-04-04 12:22:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         <label><input id="remember" type="checkbox" checked="checked" name="remember">Logindaten merken</label>
       </form>
       <p id="fehlermeldung" <?php echo $error;?>><?php echo $errorText;?></p>
-      <p>Du hast noch keinen Accoutn?</p>
+      <p>Du hast noch keinen Account?</p>
       <button id="regiBtn" onclick="document.getElementById('register').style.display='block'">Registrieren</button>
 
       <div id="register" class="modal">
@@ -34,17 +34,17 @@
           <hr>
           <div>          
             <label for="vn"><b>Vorname</b></label>
-            <input type="text" placeholder="Enter firstname" name="vn" id="vn" required>
+            <input type="text" placeholder="Gib Vorname ein" name="vn" id="vn" required>
           </div>
 
           <div>          
             <label for="nn"><b>Nachname</b></label>
-            <input type="text" placeholder="Enter name" name="nn" id="nn" required>
+            <input type="text" placeholder="Gib Nachname ein" name="nn" id="nn" required>
           </div>
 
           <div>
             <label for="un"><b>Username</b></label><span></span>
-            <input type="text" placeholder="Enter username" name="un" id="un" required>
+            <input type="text" placeholder="Gib Username ein" name="un" id="un" required>
           </div>
           
           <div>
@@ -54,22 +54,22 @@
 
           <div>
             <label for="email"><b>Email</b></label><span></span>
-            <input type="email" placeholder="Enter email" name="email" id="email" required>
+            <input type="email" placeholder="Gib Email ein" name="email" id="email" required>
           </div>
 
           <div>
             <label for="telNr"><b>Telefonnummer</b></label><span></span>
-            <input type="text" placeholder="Enter phonenumber" name="telNr" id="telNr" required>
+            <input type="text" placeholder="Gib Telefonnummer ein" name="telNr" id="telNr" required>
           </div>
 
           <div>
             <label for="psw"><b>Passwort</b></label>
-            <input type="password" placeholder="Enter password" name="psw" id="psw" required>
+            <input type="password" placeholder="Gib Passwort ein" name="psw" id="psw" required>
           </div>
 
           <div>
             <label for="psw-repeat"><b>Passwort wiederholen</b></label><span></span>
-            <input type="password" placeholder="Enter repeat password" name="psw-repeat" id="psw-repeat" required>
+            <input type="password" placeholder="Wiederhole das Passwort" name="psw-repeat" id="psw-repeat" required>
           </div>
           <hr>
 
